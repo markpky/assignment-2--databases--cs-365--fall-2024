4e6266147ba932f011251f461fa8a05071b77b03
markpky <amar.singh.eshri@gmail.com>
2024-10-14 13:04:56 -0400

    Created people table
    Made the people table to hold people who have passwords.

--- setup.sql
+++ setup.sql
@@ -3,3 +3,14 @@ DROP DATABASE IF EXISTS passwords;
 CREATE DATABASE passwords;
 
 USE passwords;
+
+CREATE TABLE passwords.people (
+  firstName varchar(50) NOT NULL COMMENT 'The first name of a person.',
+  lastName varchar(50) NOT NULL COMMENT 'The last name of a person.',
+  email varchar(320) NOT NULL COMMENT 'The email of a person. No two people within the passwords database can use the same email.',
+  comment varchar(60) NULL COMMENT 'A little comment about the person.',
+  CONSTRAINT peoplePK PRIMARY KEY (email)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
