acab3b8d5ef1e8a8572b4e977161c4419afece27
markpky <amar.singh.eshri@gmail.com>
2024-10-14 23:02:34 -0400

    Removed code from DBeaver
    DBeaver adds some extra lines of code when creating tables that doesn't seem necessary so I got rid of it.

--- setup.sql
+++ setup.sql
@@ -10,16 +10,10 @@ CREATE TABLE IF NOT EXISTS passwords.people (
   email varchar(320) NOT NULL COMMENT 'The email of a person. No two people within the passwords database can use the same email.',
   comment varchar(60) NULL COMMENT 'A little comment about the person.',
   CONSTRAINT peoplePK PRIMARY KEY (email)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE IF NOT EXISTS passwords.websites (
   name varchar(50) NOT NULL COMMENT 'Name of the website.',
   url varchar(2000) NOT NULL COMMENT 'URL of the website.',
   CONSTRAINT websitesPK PRIMARY KEY (url)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+);
