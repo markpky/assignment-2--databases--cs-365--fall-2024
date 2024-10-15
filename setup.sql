0068446810dfe95a81148a85380634ac1b9e67e1
markpky <amar.singh.eshri@gmail.com>
2024-10-15 00:03:21 -0400

    Changed primary keys
    Changed primary keys for people and websites.
    
    It might be a bit tricky to make the primary key something that can be modified, and the URL while unique to only website is just too long.

--- setup.sql
+++ setup.sql
@@ -5,15 +5,17 @@ CREATE DATABASE passwords;
 USE passwords;
 
 CREATE TABLE IF NOT EXISTS passwords.people (
+  personID smallint(5) NOT NULL COMMENT 'Unique identifier for a person.',
   firstName varchar(50) NOT NULL COMMENT 'The first name of a person.',
   lastName varchar(50) NOT NULL COMMENT 'The last name of a person.',
   email varchar(320) NOT NULL COMMENT 'The email of a person. No two people within the passwords database can use the same email.',
   comment varchar(60) NULL COMMENT 'A little comment about the person.',
-  CONSTRAINT peoplePK PRIMARY KEY (email)
+  CONSTRAINT peoplePK PRIMARY KEY (personID)
 );
 
 CREATE TABLE IF NOT EXISTS passwords.websites (
+  websiteID smallint(5) NOT NULL COMMENT 'Unique identifier for a website',
   name varchar(50) NOT NULL COMMENT 'Name of the website.',
   url varchar(2000) NOT NULL COMMENT 'URL of the website.',
-  CONSTRAINT websitesPK PRIMARY KEY (url)
+  CONSTRAINT websitesPK PRIMARY KEY (websiteID)
 );
