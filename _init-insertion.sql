d53a22a2d94049ac0df254063e52dae85088312e
markpky <amar.singh.eshri@gmail.com>
2024-10-15 23:08:04 -0400

    Website insertion
    The websites that are initially inserted have been added to the init-insertion file.

--- _init-insertion.sql
+++ _init-insertion.sql
@@ -7,3 +7,11 @@ INSERT INTO people
   (personID, firstName, lastName, email)
 VALUES
   (3, "John", "Doe", "@yahoo.com");
+
+INSERT INTO websites
+  (websiteID, name, url, comment)
+VALUES
+  (1, "Roblox", "https://www.roblox.com/", "Powering Imagination"),
+  (2, "John's Donuts", "http://www.johnsdonuts.com/", "An anonymous owner sells donuts"),
+  (3, "YouTube", "https://www.youtube.com/", "Broadcast yourself"),
+  (4, "Wizard101", "https://www.wizard101.com/", "Play The Ultimate Wizard Game Today");
