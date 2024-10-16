6a20f93054f59437d4652b153d15bb7fc42f84e1
markpky <amar.singh.eshri@gmail.com>
2024-10-16 02:00:31 -0400

    Third command
    Third command complete.

--- commands.sql
+++ commands.sql
@@ -8,3 +8,6 @@ VALUES
   (4, 4, "FelMaster99", AES_ENCRYPT("magicsrulez43248", @key_str, @init_vector), "2009-02-01 14:33:08", "all stats maxxed");
 
 SELECT CAST(AES_DECRYPT(password, @key_str, @init_vector) AS CHAR) AS "Passwords" FROM users WHERE websiteID = 4;
+
+SELECT CAST(AES_DECRYPT(password, @key_str, @init_vector) AS CHAR) AS "Passwords" FROM users
+JOIN websites WHERE (users.websiteID = websites.websiteID) AND url LIKE "https%" LIMIT 2;
